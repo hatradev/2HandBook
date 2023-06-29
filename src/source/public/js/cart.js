dd4946ccb2801c0581c08bea63f3e6cfb6abcec1
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2023-06-29 17:50:29 +0700

    Refactor code of cart screen

--- src/source/public/js/cart.js
+++ src/source/public/js/cart.js
@@ -8,6 +8,8 @@ const cartProductBtns = document.querySelectorAll('.cart a.btn');
 const productItemCloseBtns = document.querySelectorAll(
   '.cart .product-item .product-close-btn'
 );
+
+// Check if the cart is empty or not, and then display the text, remove the button view and pay
 const checkEmptyCart = () => {
   if (cartList.children.length === 0) {
     cartList.innerHTML = `<p class="cart-empty-text text-center mt-5 ">You don't have any products in your cart !</p>`;
@@ -18,24 +20,42 @@ const checkEmptyCart = () => {
   }
 };
 
-cartBtn.addEventListener('click', () => {
+// Turn on the cart screen
+const turnOnCartScreen = () => {
   checkEmptyCart();
   cartScreen.classList.add('d-flex');
   setTimeout(() => {
     cartSlider.classList.add('slide');
   });
-});
+};
 
-cartCloseBtn.addEventListener('click', () => {
+// Turn off the cart screen
+const turnOffCartScreen = () => {
   cartSlider.classList.remove('slide');
   setTimeout(() => {
     cartScreen.classList.remove('d-flex');
   }, 500);
-});
+};
 
+//  ================ Add event listener  =================
+// For each of product item close button
 productItemCloseBtns.forEach((productItemCloseBtn) => {
   productItemCloseBtn.addEventListener('click', () => {
     productItemCloseBtn.parentElement.parentElement.remove();
     checkEmptyCart();
   });
+});
+
+// For the cart button on the header
+cartBtn.addEventListener('click', turnOnCartScreen);
+
+// For the cart close button in the cart slider
+cartCloseBtn.addEventListener('click', turnOffCartScreen);
+
+// For the overlay on the left of the cart slider
+cartScreen.addEventListener('click', turnOffCartScreen);
+
+// For stopping bubbling from the child elements in the cart slider
+cartSlider.addEventListener('click', (e) => {
+  e.stopPropagation();
 });
