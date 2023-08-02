3e808ec960fe53164abe3fc2d73becf498ffc9c0
dinhquytrieu <101011727+dinhquytrieu@users.noreply.github.com>
2023-08-02 15:57:59 +0700

    split sidebar-profile and statusbar-pruduct

--- src/source/app.js
+++ src/source/app.js
@@ -35,6 +35,70 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // ROUTES INIT
 route(app);
 
+app.get('/profile', (req, res) => {
+    res.render('profile_updating', { 
+      showHeader: true, showFooter: true
+     });
+  });
+
+  app.get('/becomeseller', (req, res) => {
+    res.render('become_seller', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/myorderdelivery', (req, res) => {
+    res.render('my_order_inDelivery', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/myorderconfirmation', (req, res) => {
+    res.render('my_order_inConfirmation', {
+      showHeader: true,
+      showFooter: true,
+    });
+  });
+
+  app.get('/myordercanceled', (req, res) => {
+    res.render('my_order_canceled', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/myorder', (req, res) => {
+    res.render('my_order', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/test', (req, res) => {
+    res.render('test', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/all-product', (req, res) => {
+    res.render('all-product', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/specific-product', (req, res) => {
+    res.render('specific-product', {
+      showHeader: true,
+      showFooter: true,
+    });
+  });
+
+  app.get('/manage-order', (req, res) => {
+    res.render('manage-order', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/shop-info', (req, res) => {
+    res.render('shop-info', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/manage-product', (req, res) => {
+    res.render('manage-product', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/edit-product', (req, res) => {
+    res.render('edit-product', { showHeader: true, showFooter: true });
+  });
+
+  app.get('/dashboard', (req, res) => {
+    res.render('dashboard', { showHeader: true, showFooter: true });
+  });
+
 app.get('/payment', (req, res) => {
   res.render('payment', { showHeader: true, showFooter: true });
 });
--- src/source/server.js
+++ src/source/server.js
@@ -1,7 +1,7 @@
 const database = require('../config/db.config');
 const app = require('./app');
 
-database.connect();
+// database.connect();
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log(`App is running on port ${port} ...`);
