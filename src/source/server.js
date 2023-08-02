45712d3d38c08d4d8c9d9256936219b752e48cac
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2023-08-02 12:12:22 +0700

    Merge Tuan2

--- src/source/server.js
+++ src/source/server.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+// dotenv.config({ path: './config.env' });
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
 
-mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+// mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
