df6947cda6b88a84849c39f6fa8f0383e59c627b
demo1207 <100425282+demo1207@users.noreply.github.com>
2023-08-02 09:22:46 +0700

    commit UI

--- src/source/app.js
+++ src/source/app.js
@@ -91,4 +91,60 @@ app.get('/shop-info', (req, res) => {
   res.render('shop-info', { showHeader: true, showFooter: true });
 });
 
+app.get('/payment', (req, res) => {
+  res.render('payment', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_announcement', (req, res) => {
+  res.render('admin_announcement', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_all_announcement', (req, res) => {
+  res.render('admin_all_announcement', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_account_all', (req, res) => {
+  res.render('admin_account_all', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_account_pending', (req, res) => {
+  res.render('admin_account_pending', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_account_reported', (req, res) => {
+  res.render('admin_account_reported', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_account_banned', (req, res) => {
+  res.render('admin_account_banned', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_product_all', (req, res) => {
+  res.render('admin_product_all', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_product_pending', (req, res) => {
+  res.render('admin_product_pending', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_product_reported', (req, res) => {
+  res.render('admin_product_reported', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_product_banned', (req, res) => {
+  res.render('admin_product_banned', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_product_trending', (req, res) => {
+  res.render('admin_product_trending', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_comment_all', (req, res) => {
+  res.render('admin_comment_all', { showHeader: true, showFooter: true });
+});
+
+app.get('/admin_comment_reported', (req, res) => {
+  res.render('admin_comment_reported', { showHeader: true, showFooter: true });
+});
+
 module.exports = app;
